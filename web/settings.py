07aa110ae2361ebc487d1ecd5d3a29c35f1b2bf0
haihs-github <haihs.ptit@gmail.com>
2024-09-26 01:43:00 +0700

    deploy

--- web/settings.py
+++ web/settings.py
@@ -77,30 +77,30 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'haihs31$default',
-#         'USER': 'haihs31',
-#         'PASSWORD': 'userpass',
-#         'HOST': 'haihs31.mysql.pythonanywhere-services.com', 
-#         'PORT': '3306',
-#         'OPTIONS': {
-# 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-# }
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myhouse',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'NAME': 'haihs31$default',
+        'USER': 'haihs31',
+        'PASSWORD': 'userpass',
+        'HOST': 'haihs31.mysql.pythonanywhere-services.com', 
         'PORT': '3306',
+        'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+}
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'myhouse',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
